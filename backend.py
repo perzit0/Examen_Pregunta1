e6be1a63c277a067f460be20d05a34ec76bb740b
perzit0 <percy19marceliano@gmail.com>
2026-05-31 15:06:16 -0500

    Primer commit proyecto Flask

---
+++ backend.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+# Usuario de prueba
+USUARIO = "admin"
+PASSWORD = "1234"
+
+# Productos de prueba
+productos = {
+    "P001": {
+        "nombre": "Laptop Lenovo",
+        "precio": 2500
+    },
+    "P002": {
+        "nombre": "Mouse Logitech",
+        "precio": 80
+    },
+    "P003": {
+        "nombre": "Teclado Mecanico",
+        "precio": 150
+    }
+}
+
+# LOGIN
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+# VALIDAR USUARIO
+@app.route('/validar', methods=['POST'])
+def validar():
+
+    usuario = request.form['usuario']
+    password = request.form['password']
+
+    if usuario == USUARIO and password == PASSWORD:
+        return redirect('/principal')
+
+    return redirect('/')
+
+
+# VENTANA PRINCIPAL
+@app.route('/principal')
+def principal():
+    return render_template('principal.html')
+
+
+# BUSCADOR
+@app.route('/buscar')
+def buscar():
+    return render_template('buscador.html')
+
+
+# BUSCAR PRODUCTO
+@app.route('/buscar_producto', methods=['POST'])
+def buscar_producto():
+
+    codigo = request.form['codigo']
+
+    producto = productos.get(codigo)
+
+    return render_template(
+        'buscador.html',
+        producto=producto
+    )
+
+
+# SALIR
+@app.route('/salir')
+def salir():
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
